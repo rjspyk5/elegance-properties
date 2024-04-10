75ae55f5e332c075b464b6c9709a2275eb1b5f2a
rjspyk5 <rjspyk5@gmail.com>
2024-04-10 17:45:56 +0600

    basic errorpage created

--- src/Routing/Routes.jsx
+++ src/Routing/Routes.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Root } from "../Layout/Root";
+import { ErrorPage } from "../Componets/ErrorPage/ErrorPage";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
