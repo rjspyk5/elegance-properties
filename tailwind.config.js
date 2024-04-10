a4de0df3890cea78a5246eb4fb2f1024496878e3
rjspyk5 <rjspyk5@gmail.com>
2024-04-10 17:35:50 +0600

    custom font and custom color setup at tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "EB Garamond, Arial, sans-serif",
+    },
+    colors: {
+      main: "#111930",
+      second: "#61b22a",
+      third: "#04a5e5",
+    },
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
 };
