ab258c6d159fd038fe6b083c48d8a884f4f395c1
rjspyk5 <rjspyk5@gmail.com>
2024-04-10 18:04:30 +0600

    changed data theme and added custom color to tailwind config

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../SharedComponets/Navbar";
 
 export const Root = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["night"],
+  },
   theme: {
     fontFamily: {
       primary: "EB Garamond, Arial, sans-serif",
     },
     colors: {
-      main: "#111930",
-      second: "#61b22a",
-      third: "#04a5e5",
+      first: "#61b22a",
+      second: "#04a5e5",
+      third: "#436971",
     },
   },
 
