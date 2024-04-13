da717714866b72939df087c893a5ec4532dc2b25
rjspyk5 <rjspyk5@gmail.com>
2024-04-13 11:04:14 +0600

    added basic slider component

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Slider } from "../Componets/Slider/Slider";
+
+export const Home = () => {
+  return (
+    <div>
+      <Slider></Slider>
+    </div>
+  );
+};
--- src/Routing/Routes.jsx
+++ src/Routing/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Root } from "../Layout/Root";
 import { ErrorPage } from "../Componets/ErrorPage/ErrorPage";
+import { Home } from "../Pages/Home";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
