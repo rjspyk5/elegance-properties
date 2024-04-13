ee95fcaabaf4b0855dd333ec2396f98f64f4544f
rjspyk5 <rjspyk5@gmail.com>
2024-04-14 02:54:27 +0600

    setup login,signup,logout function on Provider context

--- src/Provider/Provider.jsx
+++ src/Provider/Provider.jsx
@@ -1,8 +1,31 @@
-import React, { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "./../Firebase/firebase.config";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+
 export const AuthContext = createContext();
+
 export const Provider = ({ children }) => {
   const [user, setuser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => setuser(user));
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const logIn = (email, pass) => signInWithEmailAndPassword(auth, email, pass);
+
+  const signUp = (email, pass) =>
+    createUserWithEmailAndPassword(auth, email, pass);
+  const logOut = () => signOut(auth);
+
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, logIn, signUp, logOut }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
--- src/SharedComponets/Navbar.jsx
+++ src/SharedComponets/Navbar.jsx
@@ -56,7 +56,7 @@ export const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52"
             >
               {menu}
             </ul>
