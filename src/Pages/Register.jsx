5a6923b3e60bec49b48b727290af63d4fcc2d378
rjspyk5 <rjspyk5@gmail.com>
2024-04-14 12:00:31 +0600

    basic login,logout,signout fuctionality implement

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,13 +1,50 @@
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/Provider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Register = () => {
+  const { user, setuser, signUp } = useContext(AuthContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const pass = form.get("password");
+    const name = form.get("name");
+    const url = form.get("url");
+
+    signUp(email, pass)
+      .then((res) => {
+        res.user.displayName = name;
+        res.user.photoURL = url;
+        successToast("User has been created successfully");
+      })
+      .catch((err) => errorToast(err));
+  };
+
+  const successToast = (typee, msz) => toast.success(msz);
+  const errorToast = (msz) => toast.error(`${msz}`);
+
   return (
     <div className="hero min-h-screen bg-base-200">
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <div className="hero-content flex-col w-full">
         <div className="card shrink-0 w-full md:w-[500px]  shadow-2xl  bg-base-100">
-          <form className="card-body w-full ">
+          <form className="card-body w-full " onSubmit={handleSubmit}>
             <h1 className="text-4xl font-bold text-first text-center">
               Registration Form
             </h1>
@@ -16,10 +53,10 @@ export const Register = () => {
                 <span className="label-text">Name</span>
               </label>
               <input
+                name="name"
                 type="text"
                 placeholder="Name"
                 className="input input-bordered"
-                required
               />
             </div>
             <div className="form-control">
@@ -27,10 +64,10 @@ export const Register = () => {
                 <span className="label-text">Photo Url</span>
               </label>
               <input
+                name="url"
                 type="text"
-                placeholder="Phot Url"
+                placeholder="Photo Url"
                 className="input input-bordered"
-                required
               />
             </div>
             <div className="form-control">
@@ -38,6 +75,7 @@ export const Register = () => {
                 <span className="label-text">Email</span>
               </label>
               <input
+                name="email"
                 type="email"
                 placeholder="Email"
                 className="input input-bordered"
@@ -49,6 +87,7 @@ export const Register = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
+                name="password"
                 type="password"
                 placeholder="Password"
                 className="input input-bordered"
