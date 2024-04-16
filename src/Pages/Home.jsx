dc8602ed0fd221bc51ea4a44111bc38291a9735d
rjspyk5 <rjspyk5@gmail.com>
2024-04-17 00:47:09 +0600

    added basic FAQ component from daisy ui

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import { Estates } from "../Componets/Estates/Estates";
 import { Slider } from "../Componets/Slider/Slider";
 import { GetInTouch } from "./../Componets/GetInTouch/GetInTouch";
+import { FAQ } from "./../Componets/FAQ/FAQ";
 
 export const Home = () => {
   return (
@@ -11,6 +12,8 @@ export const Home = () => {
       </Helmet>
       <Slider></Slider>
       <Estates />
+      <FAQ />
+
       <GetInTouch />
     </div>
   );
