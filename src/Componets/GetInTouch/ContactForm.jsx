db620c14dce0b3c94b28dd025c1c2567d40ccba5
rjspyk5 <rjspyk5@gmail.com>
2024-04-16 12:33:42 +0600

    get in touch section color chanage

--- src/Componets/GetInTouch/ContactForm.jsx
+++ src/Componets/GetInTouch/ContactForm.jsx
@@ -1,34 +1,44 @@
 export const ContactForm = () => {
-    return (
-      <div className='p-6 w-full'>
-        <form action=''>
-          <fieldset className=' flex flex-col gap-4 *:w-full *:rounded-md'>
-            {/* name and email */}
-            <h1 className='text-center'>Get in touch</h1>
-            <div className='flex gap-4 *:rounded-md *:focus:ring-c-sucess'>
-              <input
-                type='text'
-                name='Firstname'
-                placeholder='Enter your firstname'
-              />
-              <input type='email' name='Email' placeholder='Enter your email' />
-            </div>
-            {/* number */}
-            <input className='' type='number' placeholder='Enter your number' />
-            {/* textarea */}
-            <textarea
-              name=''
-              id=''
-              cols='30'
-              rows='5'
-              placeholder='Your Message Here'
-            ></textarea>
-            <button type='button' className='bg-c-info p-2'>
-              Send Message
-            </button>
-          </fieldset>
-        </form>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="p-6 w-full">
+      <form action="">
+        <fieldset className=" flex flex-col gap-4 *:w-full *:rounded-md">
+          {/* name and email */}
+          <h1 className="text-center">Get in touch</h1>
+          <div className="flex gap-4 *:rounded-md *:focus:ring-c-sucess">
+            <input
+              type="text"
+              name="Firstname"
+              className="p-2 w-full"
+              placeholder="Enter your firstname"
+            />
+            <input
+              type="email"
+              name="Email"
+              className="p-2 w-full"
+              placeholder="Enter your email"
+            />
+          </div>
+          {/* number */}
+          <input
+            className="p-2"
+            type="number"
+            placeholder="Enter your number"
+          />
+          {/* textarea */}
+          <textarea
+            name=""
+            id=""
+            className="p-2"
+            cols="30"
+            rows="5"
+            placeholder="Your Message Here"
+          ></textarea>
+          <button type="button" className="bg-first p-2">
+            Send Message
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
--- src/Componets/GetInTouch/GetInTouch.jsx
+++ src/Componets/GetInTouch/GetInTouch.jsx
@@ -7,14 +7,18 @@ export const GetInTouch = () => {
       className="grid grid-cols-1 md:grid-cols-2  py-5 my-5 gap-2 md:gap-5 text-white
 "
     >
-      <div className="shadow drop-shadow-xl shadow-[#1f1e1e58]  *:p-5 *:rounded-lg grid grid-cols-2 gap-2 md:gap-5">
-        <div className="col-span-2 bg-base-100 flex items-center justify-center ">
+      <div className="  *:p-5 *:rounded-lg grid grid-cols-2 gap-2 md:gap-5">
+        <div className="col-span-2 shadow-xl drop-shadow-2xl flex items-center justify-center ">
           <p>Regional Office</p>
         </div>
-        <div className="flex items-center bg-base-100 justify-center">f</div>
-        <div className="flex items-center bg-base-100 justify-center">f</div>
+        <div className="flex items-center shadow-xl drop-shadow-2xl justify-center">
+          f
+        </div>
+        <div className="flex items-center shadow-xl drop-shadow-2xl justify-center">
+          f
+        </div>
       </div>
-      <div className="shadow drop-shadow-xl p-5 flex justify-center items-center rounded-lg shadow-[#1f1e1e58] bg-base-100 ">
+      <div className=" p-5 flex justify-center items-center rounded-lg shadow-xl drop-shadow-2xl ">
         <ContactForm />
       </div>
     </div>
