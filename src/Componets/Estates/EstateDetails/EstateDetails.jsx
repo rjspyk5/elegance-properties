94b26c214dd9a5e6fa3f3f04ab81329ff9cc2d01
rjspyk5 <rjspyk5@gmail.com>
2024-04-17 17:04:50 +0600

    update estate details title style

--- src/Componets/Estates/EstateDetails/EstateDetails.jsx
+++ src/Componets/Estates/EstateDetails/EstateDetails.jsx
@@ -14,12 +14,15 @@ export const EstateDetails = () => {
       <figure>
         <img className="rounded-lg" src={estate.image} alt="car!" />
       </figure>
-      <div>
-        <h1>{estate.estate_title}</h1>
+      <div className="space-y-3 p-2">
+        <span className="px-2 rounded-xl py-1 bg-[#0bf7892c]">
+          Status: {estate.status}
+        </span>
+        <h1 className="font-bold text-3xl">{estate.estate_title}</h1>
+        <p>Location: {estate.description}</p>
         <p>Description: {estate.description}</p>
         <p>Type: {estate.segment_name}</p>
-        <p>Status: {estate.status}</p>
-        <p>Location: {estate.description}</p>
+
         <p>Area: {estate.area}</p>
         <p>Price: {estate.price}</p>
         <p>Facilites: {estate.facilities}</p>
